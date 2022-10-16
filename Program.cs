659fe9214eef0e82292dc6966774a4759e36cbd0
gharjare <gharjarechetan@gmail.com>
2022-10-16 14:24:19 +0530

    assignment prblm

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using AssignmentProblems;
+
+namespace AssignmentProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Flipcoin.CheckPercentage();
+            //LeapYear.CheckYear();
+            HarmonicSeries.CheckHarmonic();
+            //Factor.CheckPrimeFactor();
+
+        }
+    }
+}
